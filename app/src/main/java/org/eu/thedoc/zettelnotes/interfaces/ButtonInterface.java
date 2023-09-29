f8910816e9118f633d92419265cee32a69a5db42
Rohit <rohitsharma.royal@gmail.com>
2023-09-29 22:53:59 +0530

    + add bool to return all text if none selected

--- app/src/main/java/org/eu/thedoc/zettelnotes/interfaces/ButtonInterface.java
+++ app/src/main/java/org/eu/thedoc/zettelnotes/interfaces/ButtonInterface.java
@@ -1,7 +1,6 @@
 package org.eu.thedoc.zettelnotes.interfaces;
 
 import android.content.Intent;
-
 import androidx.activity.result.ActivityResult;
 
 public abstract class ButtonInterface {
@@ -13,29 +12,37 @@ public abstract class ButtonInterface {
 
   protected Callback mCallback;
 
-  public abstract String getName ();
+  public abstract String getName();
 
-  public abstract Listener getListener ();
+  public abstract Listener getListener();
 
-  public void registerCallback (Callback callback) {
+  public void registerCallback(Callback callback) {
     mCallback = callback;
   }
 
   public interface Listener {
-    void onClick ();
-    boolean onLongClick ();
+
+    void onClick();
+
+    boolean onLongClick();
   }
 
   public interface ActivityResultListener {
-    void getActivityResult (ActivityResult result);
+
+    void getActivityResult(ActivityResult result);
   }
 
   public interface Callback {
-    void startActivityForResult (Intent intent);
-    void setActivityResultListener (ActivityResultListener result);
-    void insertText (String text);
-    void replaceTextSelected (String text);
-    String getTextSelected ();
+
+    void startActivityForResult(Intent intent);
+
+    void setActivityResultListener(ActivityResultListener result);
+
+    void insertText(String text);
+
+    void replaceTextSelected(String text);
+
+    String getTextSelected(boolean returnAllIfEmpty);
   }
 
 }
